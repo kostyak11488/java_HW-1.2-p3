a9e4edc9a0014e9c13f6994be19fd8692d20a139
Kostya Yakovlev <akovlevkosta18@gmail.com>
2025-12-23 20:32:58 +0300

    initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,34 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldReturnSentData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+
+    @Test
+    public void shouldReturnRussianText() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("тестовые данные")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("тестовые данные"));
+    }
+}
